5cd22b83b158debf86777c3dec1b9257c1f5e39c
rafasalgado <rasalgadol@unal.edu.co>
2018-02-20 17:22:26 -0500

    avance clase

---
+++ taller1.py
@@ -0,0 +1,31 @@
+import sys
+
+
+alf = list(map(str, sys.stdin.readline().strip().split(' ')))
+print (alf)
+q = list(map(str,  sys.stdin.readline().strip().split(' ')))
+
+q0= input(" introduce el estado inicial ")
+qf = list(map(str,  sys.stdin.readline().strip().split(' ')))
+
+mt={}
+
+
+for i in range(len(q)):
+    for j in range(len(alf)):
+        print ("del estado " + q[i] + " con el simbolo " + alf[j] + " a que estado llega")
+        aux = input()
+
+        if q[i] in mt:
+            mt[q[i]][alf[j]] = aux
+
+        else:
+            mt[q[i]]= {alf[j]:aux}
+print ("ingrese cadena a evaluar ")
+
+stra= input ()
+stat= mt.key()
+valu = mt.values()
+
+for i in (stra):
+    print(i)
