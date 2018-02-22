5353858379e2b6d6eaa4596e3fc0e095602412a2
rafasalgado <rasalgadol@unal.edu.co>
2018-02-21 19:25:15 -0500

    final

--- taller1.py
+++ taller1.py
@@ -4,12 +4,13 @@ print (" introduce un alfabeto,  separado por espacios: ")
 alf = list(map(str, sys.stdin.readline().strip().split(' ')))
 print (alf)
 
-print ("estados (separados por espacios): ")
+print ("ingrese estados (separados por espacios): ")
 q = list(map(str,  sys.stdin.readline().strip().split(' ')))
 print (q)
 
-q0= input(" introduce el estado inicial: ")
-print (q0)
+print (" introduce el estado inicial: ")
+q0= input()
+
 
 print (" estado final/es separados por espacios: ")
 qf = list(map(str,  sys.stdin.readline().strip().split(' ')))
@@ -20,7 +21,7 @@ mt={}
 
 for i in range(len(q)):
     for j in range(len(alf)):
-        print ("del estado " + q[i] + " con el simbolo " + alf[j] + " a que estado llega")
+        print ("del estado " + q[i] + " con el simbolo '" + alf[j] + "' a que estado llega")
         aux = input()
 
         if q[i] in mt:
@@ -28,14 +29,24 @@ for i in range(len(q)):
 
         else:
             mt[q[i]]= {alf[j]:aux}
-print ("ingrese cadena a evaluar ")
 
-stra= input ()
+while 1:
+    print ("\n ingrese cadena a evaluar por el automata")
 
-state = q0
-for i in stra:
-    state = mt[state][i]
-if state in qf:
-    print (" cadena aceptada, FELICIDADES! ")
-else:
-    print (" cadena no aceptada ")
+    stra= input ()
+
+    state = q0
+    for i in stra:
+        state = mt[state][i]
+    if state in qf:
+        print (" cadena aceptada, MUY BIEN! ")
+    else:
+        print (" cadena no aceptada ")
+
+    print("")
+    print("desea evaluar otra cadena (y/n)")
+    opt = input()
+    if opt == "y":
+        continue
+    else:
+        break
