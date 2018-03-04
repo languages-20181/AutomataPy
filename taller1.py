2f03d4088579e202885f97cdfb76ff16e9cd9feb
rafasalgado <rasalgadol@unal.edu.co>
2018-03-04 18:06:36 -0500

    se implementa la funcionalidad para generar el grafico del automata

--- taller1.py
+++ taller1.py
@@ -1,4 +1,7 @@
 import sys
+from graphviz import Digraph
+
+g= Digraph(comment='estructura automata')
 
 print (" introduce un alfabeto,  separado por espacios: ")
 alf = list(map(str, sys.stdin.readline().strip().split(' ')))
@@ -7,6 +10,10 @@ print (alf)
 print ("ingrese estados (separados por espacios): ")
 q = list(map(str,  sys.stdin.readline().strip().split(' ')))
 print (q)
+
+for i in q:
+    g.node(i)
+
 
 print (" introduce el estado inicial: ")
 q0= input()
@@ -23,12 +30,16 @@ for i in range(len(q)):
     for j in range(len(alf)):
         print ("del estado " + q[i] + " con el simbolo '" + alf[j] + "' a que estado llega")
         aux = input()
+        g.edge(q[i], aux, alf[j])
 
         if q[i] in mt:
             mt[q[i]][alf[j]] = aux
 
+
         else:
             mt[q[i]]= {alf[j]:aux}
+
+print(g.source)
 
 while 1:
     print ("\n ingrese cadena a evaluar por el automata")
@@ -39,12 +50,14 @@ while 1:
     for i in stra:
         state = mt[state][i]
     if state in qf:
+        print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
         print (" cadena aceptada, MUY BIEN! ")
+        print ("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+        g.render('test-output/round-table.gv', view=True)
     else:
-        print (" cadena no aceptada ")
+        print (" cadena no aceptada \n")
 
-    print("")
-    print("desea evaluar otra cadena (y/n)")
+    print("Desea evaluar otra cadena (y/n)")
     opt = input()
     if opt == "y":
         continue
